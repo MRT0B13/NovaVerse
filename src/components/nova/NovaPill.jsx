67835abc4081f0bf5f25d24597d42f3a64bfbe53
Mr Tobias <mr.shavaun@gmail.com>
2026-03-10 20:50:51 +0000

    File changes

---
+++ src/components/nova/NovaPill.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function NovaPill({ text, color = '#c084fc' }) {
+  return (
+    <span
+      className="font-mono text-[10px] uppercase tracking-wider px-2 py-[2px] rounded-[3px] inline-flex items-center"
+      style={{
+        color,
+        backgroundColor: color + '18',
+        border: `1px solid ${color}40`,
+      }}
+    >
+      {text}
+    </span>
+  );
+}
